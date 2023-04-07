70c5f033b54d6239c70a14cc3beaa960fa61b2d5
bertolima <bertolilima27@gmail.com>
2023-04-07 00:07:20 -0300

    n

--- Data structure and algorithms/EDA - Monitoria/lab01/cSetor.py
+++ Data structure and algorithms/EDA - Monitoria/lab01/cSetor.py
@@ -1,5 +1,4 @@
 import pyglet
-from pyglet import shapes
 import random
 
 class Setor:
@@ -40,80 +39,45 @@ class Setor:
         return self.calcArea()
     
     def contains(self, setor):
-        minX = self.x
-        maxX = self.x+self.largura
-        minY = self.y 
-        maxY = self.y+self.altura
 
-        a = (setor.x, setor.y + setor.altura)
-        b = (setor.x + setor.largura, setor.y + setor.altura)
-        c = (setor.x + setor.largura, setor.y)
-        d = (setor.x, setor.y)
+        d1 = (self.x, self.y)
+        b1 = (self.x+self.largura, self.y+self.altura)
+        b2 = (setor.x + setor.largura, setor.y + setor.altura)
+        d2 = (setor.x, setor.y)
 
-        if (((minX < a[0] < maxX) and (minY < a[1] < maxY)) or
-            ((minX < b[0] < maxX) and (minY < b[1] < maxY)) or
-            ((minX < c[0] < maxX) and (minY < c[1] < maxY)) or
-            ((minX < d[0] < maxX) and (minY < d[1] < maxY))):
-            self.hasSobreposicao = True
-            return True
-        return False
+        return ((d1[0] < b2[0] and b1[0] > d2[0]) and
+                (b1[1] > b2[1] and d1[1] < b2[1]))
     
     def makeShape(self, batch):
         return pyglet.shapes.Rectangle(self.x, self.y,self.largura, self.altura, color=(random.randint(0,255), random.randint(0,255), random.randint(0,255)), batch=batch)
     
     def getAreaSobreposta(self, setor):
-        minX = self.x
-        maxX = self.x+self.largura
-        minY = self.y 
-        maxY = self.y+self.altura
+        d1 = (self.x, self.y)
+        b1 = (self.x+self.largura, self.y+self.altura)
+        b2 = (setor.x + setor.largura, setor.y + setor.altura)
+        d2 = (setor.x, setor.y)
 
-        a = (setor.x, setor.y + setor.altura)
-        b = (setor.x + setor.largura, setor.y + setor.altura)
-        c = (setor.x + setor.largura, setor.y)
-        d = (setor.x, setor.y)
+        x1 = max(d1[0], d2[0])
+        y1 = max(d1[1], d2[1])
+        x2 = min(b1[0], b2[0])
+        y2 = min(b1[1], b2[1])
 
-        if (((minX < a[0] < maxX) and (minY < a[1] < maxY)) and 
-            ((minX < d[0] < maxX) and (minY < d[1] < maxY)) and
-            ((minX < b[0] < maxX) and (minY < b[1] < maxY)) and
-            ((minX < c[0] < maxX) and (minY < c[1] < maxY))):
-            return a[0], a[1], c[0], c[1]
-        
-        if (((minX < a[0] < maxX) and (minY < a[1] < maxY)) and
-            ((minX < d[0] < maxX) and (minY < d[1] < maxY))):
-            return d[0], d[1], maxX, b[1]
-        
-        if (((minX < b[0] < maxX) and (minY < b[1] < maxY)) and
-            ((minX < c[0] < maxX) and (minY < c[1] < maxY))):
-            return minX, d[1], b[0], b[1]
-        
-        if (((minX < a[0] < maxX) and (minY < a[1] < maxY)) and
-            ((minX < b[0] < maxX) and (minY < b[1] < maxY))):
-            return a[0], minY, b[0], b[1]
-         
-        if (((minX < d[0] < maxX) and (minY < d[1] < maxY)) and
-            ((minX < c[0] < maxX) and (minY < c[1] < maxY))):
-            return d[0], d[1], b[0], maxY
-                    
-        if ((minX < a[0] < maxX) and (minY < a[1] < maxY)):
-            return a[0], minY, maxX, b[1]
-        
-        if ((minX < b[0] < maxX) and (minY < b[1] < maxY)):
-            return minX, minY, b[0], b[1]
-        
-        
-        if ((minX < c[0] < maxX) and (minY < c[1] < maxY)):
-            return minX, d[1], b[0], maxY
-        
-        if ((minX < d[0] < maxX) and (minY < d[1] < maxY)):
-            return d[0], d[1], maxX, maxY
+        return x1, y1, x2, y2
         
         
     def makeShapeSobreposta(self, setor, batch):
         x1, y1, x2, y2 = self.getAreaSobreposta(setor)
+        self.hasSobreposicao = True
         width = x2-x1
         height = y2-y1
         self.areaSobreposicao = self.areaSobreposicao + width*height
         return pyglet.shapes.Rectangle(x1, y1, width, height, color=(255, 255, 255), batch=batch)
+    
+    def getSobreposta(self):
+        return self.areaSobreposicao
+    
+    def isSobreposto(self):
+        return self.hasSobreposicao
     
         
 
