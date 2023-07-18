2cd15f955f1ca386d02c8dfe0d6277357cf56c63
bertolilima <bertolilima27@gmail.com>
2023-07-18 12:19:34 -0300

    added some features

--- Data structure and algorithms/EDA - Monitoria/trab03/Triangle.py
+++ Data structure and algorithms/EDA - Monitoria/trab03/Triangle.py
@@ -1,7 +1,7 @@
 import pyglet
 
 class Triangle:
-    def __init__(self, x1, y1, x2, y2, x3, y3, depth):
+    def __init__(self, x1:float, y1:float, x2:float, y2:float, x3:float, y3:float, depth:int):
         #pontos que formam os vertices do triangulo
         self.x1 = x1
         self.y1 = y1
@@ -9,43 +9,44 @@ class Triangle:
         self.y2 = y2
         self.x3 = x3
         self.y3 = y3
+        self.xm = None
+        self.ym = None
+
+        #pointer pra Null
+        self.erro = None
 
         #profundidade do nó atual
         self.depth = depth
 
-        #filhos do nó atual
+        #pointer pros filhos direito e esquerdo
         self.left = None
         self.right = None
     
     #divide o triangulo de forma recursiva
     def divide(self, depth):
         # se a profundidade atual for maior que a profundidade desejada paramos de dividir
-        if (self.depth > depth): return
+        if (self.depth >= depth): return
 
         #o vertice correspondente ao angulo de 90 graus dos triangulos filhos é obtido dividindo a hipotenusa do triangulo atual por 2
-        xm = (self.x1+self.x2)/2
-        ym = (self.y1+self.y2)/2
+        self.xm = (self.x1+self.x2)/2
+        self.ym = (self.y1+self.y2)/2
 
         #criando os filhos
-        self.left = Triangle(self.x1, self.y1, self.x3, self.y3, xm, ym, self.depth+1)
-        self.right = Triangle(self.x3, self.y3, self.x2, self.y2, xm, ym, self.depth+1)
+        self.left = Triangle(self.x1, self.y1, self.x3, self.y3, self.xm, self.ym, self.depth+1)
+        self.right = Triangle(self.x3, self.y3, self.x2, self.y2, self.xm, self.ym, self.depth+1)
 
         #dando continuidade ao processo de subdivisao de forma recursiva
         self.left.divide(depth)
         self.right.divide(depth)
+        
     def subdivide(self):
-        xm = (self.x1+self.x2)/2
-        ym = (self.y1+self.y2)/2
+        self.xm = (self.x1+self.x2)/2
+        self.ym = (self.y1+self.y2)/2
 
         #criando os filhos
-        self.left = Triangle(self.x1, self.y1, self.x3, self.y3, xm, ym, self.depth+1)
-        self.right = Triangle(self.x3, self.y3, self.x2, self.y2, xm, ym, self.depth+1)
+        self.left = Triangle(self.x1, self.y1, self.x3, self.y3, self.xm, self.ym, self.depth+1)
+        self.right = Triangle(self.x2, self.y2, self.x3, self.y3, self.xm, self.ym, self.depth+1)
     
-    def getLeft(self):
-        return self.left
-    
-    def getRight(self):
-        return self.right
     
     #essa função serve pra verificar se um ponto está contido dentro do triangulo
     def contains(self, x, y):
@@ -58,7 +59,7 @@ class Triangle:
         return False
 
     #de forma resumida, calcula o desvio padrao dos pixels do triangulo, se o desvio padrao for alto continuamos a dividir, se for baixo paramos
-    def calcularIntensidadeMedia(self, pixel_data, imgWidth, imgHeight, flag):
+    def calcularIntensidadeMedia(self, pixel_data, imgWidth):
         eMin = 256
         eMax = 0
         sum = 0
@@ -70,8 +71,8 @@ class Triangle:
         y1 = int(max(self.y1, self.y2, self.y3))
 
         #loop que faz o calculo do numero de pixels percorridos e o valor total da intensidade desses pixels
-        for x in range(x3+1, x2):
-            for y in range(y3+1, y1):
+        for x in range(x3+1, x2, 2):
+            for y in range(y3+1, y1, 2):
                 if (self.contains(x,y)):
                     index = y * imgWidth + x
                     intensidade = pixel_data[index]
@@ -86,28 +87,24 @@ class Triangle:
         else:
             eMed = sum/nPixel
 
-        if (abs(eMin - eMed) > flag):return False
-        if (abs(eMax - eMed) > flag):return False
-        if (abs(eMax - eMin) > flag):return False
-        return True
+        self.erro = max(abs(eMin - eMed), abs(eMax - eMed))
     
     #desenha a arvore toda
     def drawFull(self, maxDepth, shapes, batch):
-        if (self.depth > maxDepth): return
+        if (self.depth == maxDepth): return
         if(self.depth == 0):
-            shapes.append(pyglet.shapes.Line(self.x1,self.y1,self.x2,self.y2, color =(255, 100, 100), batch=batch))
+            shapes.append(pyglet.shapes.Line(0,abs(self.x1-self.x3),abs(self.x1-self.x3),0, color =(255, 100,100,255), batch=batch))
         else:
-            shapes.append(pyglet.shapes.Line(self.x1,self.y1,self.x3,self.y3, color =(255, 100, 100), batch=batch))
+            shapes.append(pyglet.shapes.Line(self.xm,self.ym,self.x3,self.y3, color =(255, 100, 100), batch=batch))
         self.left.drawFull(maxDepth, shapes, batch)
         self.right.drawFull(maxDepth, shapes, batch)
 
-    def drawShape(self, batch):
-       return pyglet.shapes.Line(self.x1,self.y1,self.x3,self.y3, color =(255, 255,255,255), batch=batch)
+
 
     #desenha a arvore por aproximação
     def drawLevel(self, maxDepth, shapes, batch, pixelList, imgWidth, imgHeight, flag):
         if (self.depth > maxDepth): return
-        if (self.calcularIntensidadeMedia(pixelList, imgWidth, imgHeight, flag)): return
+        if (self.erro < flag): return
         if(self.depth == 0):
             shapes.append(pyglet.shapes.Line(self.x1,self.y1,self.x2,self.y2, color =(255, 100, 100), batch=batch))
         else:
@@ -141,6 +138,23 @@ class Triangle:
             self.right = Triangle(self.x3, self.y3, self.x2, self.y2, xm, ym, self.depth+1)
             return
         
+    def createShape(self, batch):
+       return pyglet.shapes.Line(self.xm,self.ym,self.x3,self.y3, color =(255, 255,255,255), batch=batch)
+        
+    def getErro(self):
+        return self.erro
+    
+    def getLeft(self):
+        return self.left
+    
+    def getRight(self):
+        return self.right
+    
+    def getDepth(self):
+        return self.depth
+
+    def getSide(self):
+        return self.side
 
 
 
--- Data structure and algorithms/EDA - Monitoria/trab03/binaryTree.py
+++ Data structure and algorithms/EDA - Monitoria/trab03/binaryTree.py
@@ -1,79 +1,103 @@
-from pyglet import *
+import pyglet
 from Triangle import Triangle
 
 
 class binaryTree:
-	def __init__(self, width, height):
-		self.height = height
-		self.width = width
-		self.left = None
-		self.right = None
-		self.depth = None
+	def __init__(self, width:float, height:float, lista):
+		self.imgHeight = height
+		self.imgWidth = width
+		
+		self.pixelList = lista
 
-	def subdivide(self, maxDepth):
+		self.left:Triangle = None
+		self.right:Triangle = None
+
+		self.depth:int = None
+
+	#recursive methods
+	def recursiveSubdivide(self, maxDepth):
 		self.depth = maxDepth
-		self.left = Triangle(0, self.height, self.width, 0, 0, 0, 0)
-		self.right = Triangle(0, self.height, self.width, 0, self.width, self.height, 0)
+		self.left = Triangle(0, self.imgHeight, self.imgWidth, 0, 0, 0, 0)
+		self.right = Triangle(0, self.imgHeight, self.imgWidth, 0, self.imgWidth, self.imgHeight, 0)
 		self.left.divide(maxDepth)
 		self.right.divide(maxDepth)
 	
-	def subdivideIter(self, maxDepth):
+	def recursiveDrawFull(self, maxDepth, shapes, batch):
+		self.left.drawFull(maxDepth, shapes, batch)
+		self.right.drawFull(maxDepth, shapes, batch)
+
+	def recursiveDrawLevel(self, maxDepth, shapes, batch, lista, width, height, flag):
+		self.left.drawLevel(maxDepth, shapes, batch, lista, width, height, flag)
+		self.right.drawLevel(maxDepth, shapes, batch, lista, width, height, flag)
+	
+	def recursiveAddLevel(self):
+		self.depth+=1
+		self.left.addLevel()
+		self.right.addLevel()
+	
+	#iterative methods
+	def iterativeSubdivide(self, maxDepth):
 		self.depth = maxDepth
-		self.left = Triangle(0, self.height, self.width, 0, 0, 0, 0)
-		self.right = Triangle(0, self.height, self.width, 0, self.width, self.height, 0)
+		self.left = Triangle(0, self.imgHeight, self.imgWidth, 0, 0, 0, 0)
+		self.right = Triangle(0, self.imgHeight, self.imgWidth, 0, self.imgWidth, self.imgHeight, 0)
 		parent:list[Triangle] = []
 		parent.append(self.left)
 		parent.append(self.right)
 		child:list[Triangle] = []
-		for i in range(maxDepth+1):
+		for i in range(maxDepth):
 			for node in parent:
+				node.calcularIntensidadeMedia(self.pixelList, self.imgWidth)
 				node.subdivide()
 				child.append(node.getLeft())
 				child.append(node.getRight())
 			parent = child
 			child:list[Triangle] = []
 
-	def drawFullIter(self, maxDepth, shapes:list, batch):
+	def iterativeDrawFull(self, maxDepth, shapes:list, batch):
 		parent:list[Triangle] = []
 		parent.append(self.left)
 		parent.append(self.right)
+		shapes.append(pyglet.shapes.Line(0,self.imgWidth,self.imgWidth,0, color =(255, 255,255,255), batch=batch))
 		child:list[Triangle] = []
-		for i in range(maxDepth+1):
+		for i in range(maxDepth):
 			for node in parent:
-				shapes.append(node.drawShape(batch))
+				shapes.append(node.createShape(batch))
 				child.append(node.getLeft())
 				child.append(node.getRight())
 			parent = child
 			child:list[Triangle] = []
 
-
-	def drawFull(self, maxDepth, shapes, batch):
-		self.left.drawFull(maxDepth, shapes, batch)
-		self.right.drawFull(maxDepth, shapes, batch)
-
-	def drawLevel(self, maxDepth, shapes, batch, lista, width, height, flag):
-		self.left.drawLevel(maxDepth, shapes, batch, lista, width, height, flag)
-		self.right.drawLevel(maxDepth, shapes, batch, lista, width, height, flag)
-	
-	def drawLevelIter(self, maxDepth, shapes, batch, lista, width, height, flag):
+	def iterativeDrawLevel(self, maxDepth, shapes, batch, lista, width, height, flag):
 		parent:list[Triangle] = []
 		parent.append(self.left)
 		parent.append(self.right)
 		child:list[Triangle] = []
-		for i in range(maxDepth+1):
+		for i in range(maxDepth):
 			for node in parent:
-				if(not node.calcularIntensidadeMedia(lista, width, height, flag)):
-					shapes.append(node.drawShape(batch))
+				if(node.getErro() > flag):
+					shapes.append(node.createShape(batch))
 					child.append(node.getLeft())
 					child.append(node.getRight())
 			parent = child
 			child:list[Triangle] = []
-	
-	def addNivel(self):
+
+	def iterativeAddLevel(self):
 		self.depth+=1
-		self.left.addLevel()
-		self.right.addLevel()
-		
+		parent:list[Triangle] = []
+		parent.append(self.left)
+		parent.append(self.right)
+		child:list[Triangle] = []
+		for i in range(self.depth-1):
+			for node in parent:
+				child.append(node.getLeft())
+				child.append(node.getRight())
+			parent = child
+			child:list[Triangle] = []
+		for node in parent:
+			node.subdivide()
+			node.calcularIntensidadeMedia(self.pixelList, self.imgWidth)
+
+	#important acessor
 	def getDepth(self):
 		return self.depth
 
